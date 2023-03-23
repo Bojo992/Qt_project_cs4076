7a65fe53fe8f91b236b54bdfbdb7a24d61e33e1d
zed992 <93447305+zed992@users.noreply.github.com>
2023-03-23 13:52:24 +0000

    Add mainwindow.cpp

---
+++ Recipe_book/mainwindow.cpp
@@ -0,0 +1,36 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "selectpagetype.h"
+#include "recipepagetypeone.h"
+#include "recipepagetypetwo.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    SelectPageType *selectPageType = new SelectPageType();
+    connect(ui->addNewRecipeButton, &QPushButton::clicked,
+            selectPageType, &SelectPageType::show);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_quiteOption_triggered()
+{
+    this->deleteLater();
+}
+
+
+void MainWindow::on_recipeListWidget_itemClicked(QListWidgetItem *item)
+{
+    RecipePageTypeOne *recipePage = new RecipePageTypeOne(this);
+    recipePage->show();
+    this->hide();
+}
+

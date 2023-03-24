8d6f5f6511d5e179af3cbd1bdf419cd26189df2e
zed992 <93447305+zed992@users.noreply.github.com>
2023-03-24 16:59:03 +0000

    Json writer abstract class

---
+++ Recipe_book/JsonWriter.h
@@ -0,0 +1,12 @@
+//
+// Created by Boris Bobylkov on 24/03/2023.
+//
+
+#ifndef RECIPE_BOOK_JSONWRITER_H
+#define RECIPE_BOOK_JSONWRITER_H
+
+class JsonWriter {
+    virtual void write() = 0;
+};
+
+#endif //RECIPE_BOOK_JSONWRITER_H
